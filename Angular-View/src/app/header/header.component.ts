0d52f504cec3eb82f1e233b5067b5f7bf791c3e6
Vasco Sousa <1221700@isep.ipp.pt>
2024-11-04 22:34:39 +0000

    ref #139 US 6.1.1 Implementation
    - Created main page
    - Created footer and header
    - Added routing to login page
    - Configured Project

---
+++ Angular-View/src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  menuValue: boolean = false;
+  menu_icon: string = 'bi bi-list';
+  menuItems: string[] = ['Home', 'Login', 'Signup', 'Contact'];
+
+  openMenu() {
+    this.menuValue = !this.menuValue;
+    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+  }
+
+  closeMenu() {
+    this.menuValue = false;
+    this.menu_icon = 'bi bi-list';
+  }
+}
