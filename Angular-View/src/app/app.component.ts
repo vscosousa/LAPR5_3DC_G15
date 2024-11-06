099bf0e764c714841f8fbb81cd5f06dc3faf8eaf
Vasco Sousa <1221700@isep.ipp.pt>
2024-11-06 20:37:54 +0000

    ref #139 US 6.1.1 Implementation
    - Fixed routing

--- Angular-View/src/app/app.component.ts
+++ Angular-View/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
