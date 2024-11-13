d1d81639d865e41de48545f8a57452ad11ec975e
Vasco Sousa <1221700@isep.ipp.pt>
2024-11-13 21:43:25 +0000

    ref #103 US 6.2.7 Tests
    - Fixed tests structure

--- Angular-View/src/app/Components/update-patient/update-patient.component.spec.ts
+++ Angular-View/src/app/Components/update-patient/update-patient.component.spec.ts
@@ -1,16 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { UpdatePatientComponent } from './update-patient.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { PatientService } from '../../Services/patient.service';
 
 describe('UpdatePatientComponent', () => {
   let component: UpdatePatientComponent;
   let fixture: ComponentFixture<UpdatePatientComponent>;
+  let mockPatientService: any;
 
   beforeEach(async () => {
+    mockPatientService = {
+      getPatientsWithAdvancedFilter: jasmine.createSpy('getPatientsWithAdvancedFilter').and.returnValue(of([{
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+        email: 'john.doe@example.com',
+        phoneNumber: '1234567890',
+        emergencyContact: 'Jane Doe',
+        medicalConditions: 'None'
+      }])),
+      updatePatient: jasmine.createSpy('updatePatient').and.returnValue(of({}))
+    };
+
     await TestBed.configureTestingModule({
-      imports: [UpdatePatientComponent]
+      imports: [FormsModule, RouterTestingModule, HttpClientTestingModule, UpdatePatientComponent, SidebarComponent],
+      providers: [
+        { provide: PatientService, useValue: mockPatientService }
+      ]
     })
     .compileComponents();
+
+    // Mock the URL to include a medical record number
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: 'http://localhost:4200/update-patient/202411000001'
+      },
+      writable: true
+    });
 
     fixture = TestBed.createComponent(UpdatePatientComponent);
     component = fixture.componentInstance;
--- Angular-View/src/app/Components/update-staff/update-staff.component.spec.ts
+++ Angular-View/src/app/Components/update-staff/update-staff.component.spec.ts
@@ -1,14 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { UpdateStaffComponent } from './update-staff.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { StaffService } from '../../Services/staff-sevice.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('UpdateStaffComponent', () => {
   let component: UpdateStaffComponent;
   let fixture: ComponentFixture<UpdateStaffComponent>;
+  let mockStaffService: any;
+  let mockActivatedRoute: any;
 
   beforeEach(async () => {
+    mockStaffService = {
+      getSpecialization: jasmine.createSpy('getSpecialization').and.returnValue(of([])),
+      updateStaff: jasmine.createSpy('updateStaff').and.returnValue(of({}))
+    };
+
+    mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get: jasmine.createSpy('get').and.returnValue('123')
+        }
+      }
+    };
+
     await TestBed.configureTestingModule({
-      imports: [UpdateStaffComponent]
+      imports: [FormsModule, ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule, UpdateStaffComponent, SidebarComponent],
+      providers: [
+        { provide: StaffService, useValue: mockStaffService },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
     })
     .compileComponents();
 
