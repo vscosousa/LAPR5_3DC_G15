de128aad3fe664a18584c00bfec092c7b49f4986
Vasco Sousa <1221700@isep.ipp.pt>
2024-12-16 00:01:02 +0000

    ref #2 Project Configuration

--- Angular-View/src/app/Components/manage-allergies-and-conditions/create-allergies/create-allergies.component.spec.ts
+++ Angular-View/src/app/Components/manage-allergies-and-conditions/create-allergies/create-allergies.component.spec.ts
@@ -18,6 +18,7 @@ describe('CreateAllergiesComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- Angular-View/src/app/Components/manage-allergies-and-conditions/create-allergies/create-allergies.component.ts
+++ Angular-View/src/app/Components/manage-allergies-and-conditions/create-allergies/create-allergies.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AllergyService } from '../../../Services/allergy.service';
-import { string } from 'three/webgpu';
 
 @Component({
   selector: 'app-create-allergies',
